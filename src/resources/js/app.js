8492c98e9afdce432e973408ac462e7b5a705ada
rei01saito <rei01saito@gmail.com>
2022-04-04 11:31:03 +0900

    resourses/js配下のjsファイル整理。location.pathnameの値でどのモジュールを使用するか振り分ける。

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,9 +1,17 @@
-require('./bootstrap');
-import './home.js';
-import './tasks.js';
-
 require('jquery-ui/ui/widgets/draggable');
 require('jquery-ui/ui/widgets/droppable');
+require('./bootstrap');
+import { HomeEvent } from './home.js';
+import { TaskEvent } from './tasks.js';
+
+window.onload = () => {
+  let path = location.pathname;
+  if (path === "/tasks") {
+    TaskEvent();
+  } else if (path === "/") {
+    HomeEvent();
+  }
+};
 
 import Alpine from 'alpinejs';
 
