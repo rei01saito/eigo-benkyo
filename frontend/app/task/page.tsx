0341cbc4380a752eb7fec3723a4e6c05a02a2331
Rei Saito <rei01saito@gmail.com>
2024-09-19 09:26:28 +0900

    TaskCard componentの作成

--- frontend/app/task/page.tsx
+++ frontend/app/task/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { fetchTasks } from '@/features/task/api/fetchTasks'
+import TaskCard from '@/features/task/components/TaskCard'
 import { NextPage } from 'next'
 import { useEffect, useState } from 'react'
 import Modal from 'react-modal'
@@ -40,108 +41,29 @@ const Task: NextPage<TaskProps> = () => {
 
   return (
     <>
-      <div className="">
+      <div>
         <div className="font-body text-center pt-6 pb-1">
           ダブルクリックで削除!
         </div>
+
         <div className="flex h-3/5">
-          <div className="px-1 w-full task-card">
-            <div className="border rounded-lg bg-white shadow-md">
-              <div className="py-4">
-                <p className="font-body text-2xl text-center pb-3">
-                  <i className="fa-solid fa-file-pen"></i>検討中のタスク
-                </p>
-                <div className="task-index" data-priority-id="0">
-                  {pendingTask.map((task) => {
-                    return (
-                      <div
-                        key={task.id}
-                        className="task"
-                        onClick={() => handleOpenModal(task.id)}
-                      >
-                        <div className="hover:cursor-pointer mx-3 my-1 p-2 border rounded-md active:bg-gray-100 shadow">
-                          <div className="flex justify-between">
-                            <p className="text-lg">{task.title}</p>
-                          </div>
-                          <p className="hidden whitespace-pre-wrap text-sm mx-3 my-1 p-1">
-                            {task.contents}
-                          </p>
-                          <p className="hidden whitespace-pre-wrap text-xs text-right text-gray-400 mx-3 my-1 p-1">
-                            ({task.minutes}分)
-                          </p>
-                        </div>
-                      </div>
-                    )
-                  })}
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="px-1 w-full task-card">
-            <div className="border rounded-lg bg-white shadow-md">
-              <div className="py-4">
-                <p className="font-body text-2xl text-center pb-3">
-                  <i className="fa-regular fa-circle-dot"></i>実行中のタスク
-                </p>
-                <div className="task-index" data-priority-id="1">
-                  {inProgressTask.map((task) => {
-                    return (
-                      <div
-                        key={task.id}
-                        className="task"
-                        onClick={() => handleOpenModal(task.id)}
-                      >
-                        <div className="hover:cursor-pointer mx-3 my-1 p-2 border rounded-md active:bg-gray-100 shadow">
-                          <div className="flex justify-between">
-                            <p className="text-lg">{task.title}</p>
-                          </div>
-                          <p className="hidden whitespace-pre-wrap text-sm mx-3 my-1 p-1">
-                            {task.contents}
-                          </p>
-                          <p className="hidden whitespace-pre-wrap text-xs text-right text-gray-400 mx-3 my-1 p-1">
-                            ({task.minutes}分)
-                          </p>
-                        </div>
-                      </div>
-                    )
-                  })}
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="px-1 w-full task-card">
-            <div className="border rounded-lg bg-white shadow-md">
-              <div className="py-4">
-                <p className="font-body text-2xl text-center pb-3">
-                  <i className="fa-solid fa-check"></i>完了したタスク
-                </p>
-                <div className="task-index" data-priority-id="2">
-                  {completedTask.map((task) => {
-                    return (
-                      <div
-                        key={task.id}
-                        className="task"
-                        onClick={() => handleOpenModal(task.id)}
-                      >
-                        <div className="hover:cursor-pointer mx-3 my-1 p-2 border rounded-md active:bg-gray-100 shadow">
-                          <div className="flex justify-between">
-                            <p className="text-lg">{task.title}</p>
-                            <button type="button"></button>
-                          </div>
-                          <p className="hidden whitespace-pre-wrap text-sm mx-3 my-1 p-1">
-                            {task.contents}
-                          </p>
-                          <p className="hidden whitespace-pre-wrap text-xs text-right text-gray-400 mx-3 my-1 p-1">
-                            ({task.minutes}分)
-                          </p>
-                        </div>
-                      </div>
-                    )
-                  })}
-                </div>
-              </div>
-            </div>
-          </div>
+          <TaskCard
+            title="Pending"
+            tasks={pendingTask}
+            handleClick={handleOpenModal}
+          />
+
+          <TaskCard
+            title="In progress"
+            tasks={inProgressTask}
+            handleClick={handleOpenModal}
+          />
+
+          <TaskCard
+            title="Completed"
+            tasks={completedTask}
+            handleClick={handleOpenModal}
+          />
         </div>
 
         <Modal
---
+++ frontend/features/task/components/TaskCard.tsx
@@ -0,0 +1,43 @@
+import { Task } from '@/features/home/types'
+
+type Props = {
+  tasks: Task[]
+  title: string
+  handleClick: (id: number) => void
+}
+
+export const TaskCard = ({ tasks, title, handleClick }: Props) => {
+  return (
+    <div className="px-1 w-full">
+      <div className="border rounded-lg bg-white shadow-md">
+        <div className="py-4">
+          <p className="font-body text-2xl text-center pb-3">
+            <i className="fa-solid fa-file-pen"></i>
+            {title}
+          </p>
+          <div data-priority-id="0">
+            {tasks.map((task) => {
+              return (
+                <div key={task.id} onClick={() => handleClick(task.id)}>
+                  <div className="hover:cursor-pointer mx-3 my-1 p-2 border rounded-md active:bg-gray-100 shadow">
+                    <div className="flex justify-between">
+                      <p className="text-lg">{task.title}</p>
+                    </div>
+                    <p className="hidden whitespace-pre-wrap text-sm mx-3 my-1 p-1">
+                      {task.contents}
+                    </p>
+                    <p className="hidden whitespace-pre-wrap text-xs text-right text-gray-400 mx-3 my-1 p-1">
+                      ({task.minutes}分)
+                    </p>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TaskCard
