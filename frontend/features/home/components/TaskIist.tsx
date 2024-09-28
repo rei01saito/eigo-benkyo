4b631857bd3003960c49052ca233c611220368d2
Rei Saito <rei01saito@gmail.com>
2024-09-28 14:05:47 +0900

    storybookの修正と追加

--- frontend/features/home/components/TaskIist.tsx
+++ frontend/features/home/components/TaskIist.tsx
@@ -7,16 +7,13 @@ type Props = {
 
 export const TaskList = ({ tasks }: Props) => {
   return (
-    <div className="border bg-white rounded-lg p-6 m-3 h-80 w-96 overflow-y-scroll">
+    <div className="border bg-white rounded-lg p-6 m-3 h-80 w-96 overflow-y-scroll scroller shadow">
       <p className="font-bold text-2xl pb-6">タスクの指定</p>
       <ul>
         {tasks.map((task, index) => {
           return (
             <React.Fragment key={index}>
-              <li
-                className="hover:underline hover:text-gray-400 cursor-pointer pb-2 set-timer"
-                data-tasks-id="{{ $item->tasks_id }}"
-              >
+              <li className="hover:underline hover:text-gray-400 cursor-pointer pb-2 set-timer">
                 {task.title}
               </li>
               <li className="hidden">{task.contents}</li>
--- frontend/stories/Header.stories.ts
+++ frontend/stories/Header.stories.ts
@@ -3,7 +3,7 @@ import { fn } from '@storybook/test'
 import { Header } from '../components/layouts/Header'
 
 const meta = {
-  title: 'Example/Header',
+  title: 'Components/Layouts/Header',
   component: Header,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
